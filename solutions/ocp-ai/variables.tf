57789746b3066c05dd7891965f1f19d379afef90
Vipin Kumar <vipinkumar@Vipins-MacBook-Pro.local>
2025-03-26 17:43:43 +0530

    change

--- solutions/ocp-ai/variables.tf
+++ solutions/ocp-ai/variables.tf
@@ -1,14 +1,17 @@
 variable "default_worker_pool_machine_type" {
   type        = string
   description = "The machine type for worker nodes."
+  default     = "bx2.4x16"
 }
 
 variable "default_worker_pool_workers_per_zone" {
   type        = number
   description = "Number of worker nodes in each zone of the cluster."
+  default     = 2
 }
 
 variable "default_worker_pool_operating_system" {
   type        = string
   description = "The operating system installed on the worker nodes."
+  default     = "RHEL_9_64"
 }
