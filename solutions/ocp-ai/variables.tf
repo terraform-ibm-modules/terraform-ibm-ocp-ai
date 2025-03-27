248ce81783bece48753e44bb3410e19ae4b4b6ad
Vipin654 <77929205+Vipin654@users.noreply.github.com>
2025-03-27 15:53:08 +0530

    Update variables.tf

--- solutions/ocp-ai/variables.tf
+++ solutions/ocp-ai/variables.tf
@@ -19,3 +19,31 @@ variable "default_worker_pool_operating_system" {
   description = "The operating system installed on the worker nodes."
   default     = "RHEL_9_64"
 }
+
+variable "additional_worker_pools" {
+  type = list(object({
+    vpc_subnets = optional(list(object({
+      id         = string
+      zone       = string
+      cidr_block = string
+    })), [])
+    pool_name         = string
+    machine_type      = string
+    workers_per_zone  = number
+    resource_group_id = optional(string)
+    operating_system  = string
+    labels            = optional(map(string))
+    minSize           = optional(number)
+    secondary_storage = optional(string)
+    maxSize           = optional(number)
+    enableAutoscaling = optional(bool)
+    boot_volume_encryption_kms_config = optional(object({
+      crk             = string
+      kms_instance_id = string
+      kms_account_id  = optional(string)
+    }))
+    additional_security_group_ids = optional(list(string))
+  }))
+  description = "List of additional worker pools. [Learn more](https://github.com/terraform-ibm-modules/terraform-ibm-base-ocp-vpc/blob/main/solutions/fully-configurable/DA_docs.md#options-with-worker-pools)"
+  default     = []
+}
