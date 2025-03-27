b2cb7fdf97d35410f0825a12727525b5d9543e12
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-03-27 13:50:52 +0530

    refactor: updated variable info

--- solutions/ocp-ai/variables.tf
+++ solutions/ocp-ai/variables.tf
@@ -1,13 +1,17 @@
 variable "default_worker_pool_machine_type" {
   type        = string
   description = "The machine type for worker nodes."
-  default     = "bx2.4x16"
+  default     = "bx2.8x32"
 }
 
 variable "default_worker_pool_workers_per_zone" {
   type        = number
   description = "Number of worker nodes in each zone of the cluster."
   default     = 2
+  validation {
+    condition     = var.default_worker_pool_workers_per_zone >= 2
+    error_message = "OCP AI add-on requires at least 2 worker nodes in each worker pool."
+  }
 }
 
 variable "default_worker_pool_operating_system" {
