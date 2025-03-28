1230505c3d44aaa35b3f0d3b51f262fc1891e292
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-03-28 19:01:38 +0530

    fix: added storage type to have default GPU enabled

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -2,7 +2,7 @@
 variable "default_worker_pool_machine_type" {
   type        = string
   description = "Specifies the machine type for the default worker pool. This determines the CPU, memory, and disk resources available to each worker node. For Openshift AI installation, machines should have atleast 8 vcpu, 32GB RAM and GPU. Refer [IBM Cloud documentation for available machine types](https://cloud.ibm.com/docs/openshift?topic=openshift-vpc-flavors)"
-  default     = "bx2.8x32"
+  default     = "gx3.16x80.l4"
 }
 
 # tflint-ignore: all
@@ -21,6 +21,13 @@ variable "default_worker_pool_operating_system" {
   type        = string
   description = "Provide the operating system for the worker nodes in the default worker pool. Ensure that the selected operating system is compatible with your AI framework and dependencies. Refer [here](https://cloud.ibm.com/docs/openshift?topic=openshift-openshift_versions) for supported Operating Systems"
   default     = "RHEL_9_64"
+}
+
+# tflint-ignore: all
+variable "default_gpu_worker_pool_storage" {
+  type        = string
+  description = "Defines the storage configuration for GPU workloads in Red Hat OpenShift AI. This storage is critical for AI/ML workloads, ensuring optimal data processing and model training efficiency. [Learn More](https://cloud.ibm.com/docs/vpc?topic=vpc-block-storage-profiles&interface=ui#tiers)"
+  default     = "300gb.5iops-tier"
 }
 
 # tflint-ignore: all
@@ -49,13 +56,5 @@ variable "additional_worker_pools" {
     additional_security_group_ids = optional(list(string))
   }))
   description = "List of additional worker pools with custom configurations to accommodate diverse AI workload requirements within the cluster. [Learn more](https://github.com/terraform-ibm-modules/terraform-ibm-ocp-ai/blob/main/solutions/fully-configurable/DA_docs.md#options-with-worker-pools)"
-  default = [
-    {
-      pool_name         = "gpu-pool"
-      machine_type      = "gx3.16x80.l4"
-      operating_system  = "RHEL_9_64"
-      workers_per_zone  = 2
-      secondary_storage = "300gb.5iops-tier"
-    }
-  ]
+  default = []
 }
