3e987cbab8439e6515efd5b4feb0347f98584e0c
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-04-27 23:54:35 +0530

    fix: removed default_gpu_worker_pool_storage and added validation in additional wp

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -25,12 +25,7 @@ variable "existing_resource_group_name" {
 variable "default_worker_pool_machine_type" {
   type        = string
   description = "Specifies the machine type for the default worker pool. This determines the CPU, memory, and disk resources available to each worker node. For OpenShift AI installation, machines should have atleast 8 vcpu, 32GB RAM and GPU. Refer [IBM Cloud documentation for available machine types](https://cloud.ibm.com/docs/openshift?topic=openshift-vpc-flavors)"
-  default     = "gx3.16x80.l4"
-
-  validation {
-    condition     = startswith(var.default_worker_pool_machine_type, "gx2") || startswith(var.default_worker_pool_machine_type, "gx3") || startswith(var.default_worker_pool_machine_type, "gx4")
-    error_message = "The machine type must be from the 'gx2', 'gx3', or 'gx4' series. Please choose a valid machine type with GPU support."
-  }
+  default     = "bx2.8x32"
 
   validation {
     condition     = tonumber(split("x", split(".", var.default_worker_pool_machine_type)[1])[0]) >= 8
@@ -63,13 +58,6 @@ variable "default_worker_pool_operating_system" {
 }
 
 # tflint-ignore: all
-variable "default_gpu_worker_pool_storage" {
-  type        = string
-  description = "Defines the storage configuration for GPU workloads in Red Hat OpenShift AI. This storage is critical for AI/ML workloads, ensuring optimal data processing and model training efficiency. [Learn More](https://cloud.ibm.com/docs/vpc?topic=vpc-block-storage-profiles&interface=ui#tiers)"
-  default     = "300gb.5iops-tier"
-}
-
-# tflint-ignore: all
 variable "additional_worker_pools" {
   type = list(object({
     vpc_subnets = optional(list(object({
@@ -89,5 +77,38 @@ variable "additional_worker_pools" {
     additional_security_group_ids = optional(list(string))
   }))
   description = "List of additional worker pools with custom configurations to accommodate diverse AI workload requirements within the cluster. [Learn more](https://github.com/terraform-ibm-modules/terraform-ibm-ocp-ai/blob/main/solutions/fully-configurable/DA_docs.md#options-with-worker-pools)"
-  default     = []
+  default = [
+    {
+      pool_name         = "gpu"
+      machine_type      = "gx3.16x80.l4"
+      workers_per_zone  = 2
+      secondary_storage = "300gb.5iops-tier"
+      operating_system  = "RHEL_9_64"
+    },
+  ]
+
+  validation {
+    condition = alltrue([
+      for pool in var.additional_worker_pools : (
+        startswith(pool.machine_type, "gx2") ||
+        startswith(pool.machine_type, "gx3") ||
+        startswith(pool.machine_type, "gx4")
+      )
+    ])
+    error_message = "All machine types must be from the 'gx2', 'gx3', or 'gx4' series. Please choose valid machine types with GPU support."
+  }
+
+  validation {
+    condition = alltrue([
+      for pool in var.additional_worker_pools : tonumber(split("x", split(".", pool.machine_type)[1])[0]) >= 8
+    ])
+    error_message = "To install Red Hat OpenShift AI, all worker nodes in all pools must have at least an 8-core CPU."
+  }
+
+  validation {
+    condition = alltrue([
+      for pool in var.additional_worker_pools : tonumber(split("x", split(".", pool.machine_type)[1])[1]) >= 32
+    ])
+    error_message = "To install Red Hat OpenShift AI, all worker nodes in all pools must have at least 32GB memory."
+  }
 }
