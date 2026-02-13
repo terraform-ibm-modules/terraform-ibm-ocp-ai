f479604aedd01f9f901a00717eae395d684d95fa
Zikra Iqbal <35029744+zikra-iqbal@users.noreply.github.com>
2026-02-13 19:25:44 +0530

    fix(deps): update the base OCP module to `3.80.2` and remove static version validation (#123)

--- solutions/quickstart/variables.tf
+++ solutions/quickstart/variables.tf
@@ -57,20 +57,8 @@ variable "region" {
 }
 variable "openshift_version" {
   type        = string
-  description = "The version of the OpenShift cluster."
+  description = "The version of the OpenShift cluster. Any add-on versions specified in the `addons` input must be compatible with the selected OpenShift cluster version."
   default     = "4.19"
-
-  validation {
-    condition = anytrue([
-      var.openshift_version == null,
-      var.openshift_version == "default",
-      var.openshift_version == "4.19",
-      var.openshift_version == "4.18",
-      var.openshift_version == "4.16",
-      var.openshift_version == "4.17",
-    ])
-    error_message = "The specified openshift_version is not of the valid versions."
-  }
 }
 variable "cluster_name" {
   type        = string
